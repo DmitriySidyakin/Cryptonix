9621d003ae7d7c40ef6671cc7ddd82e6ab16dbdc
Dmitriy Sidyakin <sidyakind@gmail.com>
2023-06-02 15:46:23 +0300

    Добавил спериализацию

---
+++ Cryptonix/Cryptonix.ConsoleTests/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cryptonix.Security;
+
+Console.WriteLine("Testing!");
+
+#region Test 0001
+/******************************************************************************************************************/
+
+Console.WriteLine("Test #0001:");
+
+byte[] buffer_0001 = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int w, h;
+byte[,] bytesM_0001 = Spiralizer.ArrayToMartix(out w, out h, buffer_0001);
+byte[] bytes_0001 = Spiralizer.MartixToArray(w, h, bytesM_0001);
+
+for (int i = 0; i < buffer_0001.Length; i++)
+{
+    if (bytes_0001[i] == buffer_0001[i])
+    {
+        Console.WriteLine($"\tTest #0001.{i + 1}: Right.");
+    }
+    else
+    {
+        Console.WriteLine($"\tTest #0001.{i + 1}: Wrong!");
+    }
+}
+/******************************************************************************************************************/
+#endregion
+
+#region Test 0002
+/******************************************************************************************************************/
+
+Console.WriteLine("Test #0002: ");
+
+for (int i = 0; i < h; i++)
+{
+    for(int j = 0; j < w; j++)
+    {
+        Console.Write((i+1)*(j+1) == bytesM_0001.Length ? bytesM_0001[j,i] : $"{bytesM_0001[j,i]}, ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+/******************************************************************************************************************/
+#endregion
+
+#region Test 0003
+/******************************************************************************************************************/
+
+Console.WriteLine("Test #0003:");
+byte[] bytes_Spiralized_0003 = Spiralizer.Spiralize(w, h, bytesM_0001);
+
+
+Console.Write($"\tTest #0003.Initial: ");
+for (int i = 0; i < buffer_0001.Length; i++)
+{
+    Console.Write(i == buffer_0001.Length - 1 ? buffer_0001[i] : $"{buffer_0001[i]}, ");
+}
+Console.WriteLine();
+
+
+Console.Write($"\tTest #0003.Spirialized: ");
+for (int i = 0; i < bytes_Spiralized_0003.Length; i++)
+{
+    Console.Write(i == bytes_Spiralized_0003.Length - 1 ? bytes_Spiralized_0003[i] : $"{bytes_Spiralized_0003[i]}, ");
+}
+Console.WriteLine();
+/******************************************************************************************************************/
+#endregion
---
+++ Cryptonix/Cryptonix.Security/Spiralizer.cs
@@ -0,0 +1,154 @@
+﻿namespace Cryptonix.Security
+{
+    public static class Spiralizer
+    {
+        public static byte[] Spiralize(int width, int height, byte[,] matrix)
+        {
+            if (matrix.Length == 0)
+                return new byte[0];
+
+            int resultSize = matrix.Length;
+
+            byte[] result = new byte[resultSize];
+
+            bool isItTheEnd = false;
+            int startTopIndex_i = 0;
+            int endTopIndex_i = width - 1;
+            int startTopIndex_j = 0;
+            int endTopIndex_j = height - 1;
+            int currentIndex = 0;
+            while (!isItTheEnd)
+            {
+                for (int j = startTopIndex_j; j <= endTopIndex_j && currentIndex != resultSize; j++)
+                {
+                    result[currentIndex] = matrix[startTopIndex_i, j];
+                    currentIndex++;
+                }
+
+                startTopIndex_i++;
+
+                for (int i = startTopIndex_i; i <= endTopIndex_i && currentIndex != resultSize; i++)
+                {
+                    result[currentIndex] = matrix[i, endTopIndex_j];
+                    currentIndex++;
+                }
+
+                endTopIndex_j--;
+
+                for (int j = endTopIndex_j; j >= startTopIndex_j && currentIndex != resultSize; j--)
+                {
+                    result[currentIndex] = matrix[endTopIndex_i, j];
+                    currentIndex++;
+                }
+
+                endTopIndex_i--;
+
+                for (int i = endTopIndex_i; i >= startTopIndex_i && currentIndex != resultSize; i--)
+                {
+                    result[currentIndex] = matrix[i, startTopIndex_j];
+                    currentIndex++;
+                }
+
+                startTopIndex_j++;
+
+
+                if (currentIndex == resultSize)
+                {
+                    isItTheEnd = true;
+                }
+            }
+
+            return result;
+        }
+
+        public static byte[,] ArrayToMartix(out int width, out int height, byte[] bytes)
+        {
+            /* Example:
+             * bytes = 
+             *  1, 2, 3, 4, 5, 6, 7, 8
+             *  , 9
+             */
+
+            if (bytes.Length == 0)
+            {
+                width = 0;
+                height = 0;
+                return new byte[0, 0];
+            }
+
+
+            int currentWidth = bytes.Length / 2;
+            int currentHeight = bytes.Length / currentWidth;
+
+            while (currentWidth > currentHeight)
+            {
+                currentWidth = currentWidth - 1;
+                currentHeight = bytes.Length / currentWidth;
+            }
+
+            byte[,] result = new byte[currentWidth, currentHeight];
+
+            for (int i = 0, iW = 0, iH = 0; i < bytes.Length; i++)
+            {
+                result[iW, iH] = bytes[i];
+
+                iW++;
+
+                if (iW >= currentWidth)
+                {
+                    iW = 0;
+                    iH++;
+                }
+
+                if (iH >= currentHeight)
+                    iH = 0;
+            }
+            width = currentWidth;
+            height = currentHeight;
+            return result;
+        }
+
+        public static byte[] MartixToArray(int width, int height, byte[,] bytes)
+        {
+            if (bytes.Length == 0)
+            {
+                return new byte[0];
+            }
+
+            byte[] result = new byte[width * height];
+
+            int r = 0;
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    result[r] = bytes[i, j];
+                    r++;
+                }
+            }
+
+            return result;
+        }
+        /*
+        private static byte[,] Despiralize(int width, int height, byte[] matrix)
+        {
+
+        }
+        */
+
+
+
+
+        /*
+        public static byte[] Encrypt(byte[] bytes)
+        {
+
+        }
+
+        public static byte Decrypt(byte[] bytes)
+        {
+
+        }*/
+    }
+}
